faf9c13fc37d34c906339196fb3daece889dccdb
Rogerio Assiz <rogerio.assiz@uol.com.br>
2024-09-03 13:58:30 -0300

    Get Image

--- src/main/java/com/assiz/cursos/imageliteapi/domain/service/ImageService.java
+++ src/main/java/com/assiz/cursos/imageliteapi/domain/service/ImageService.java
@@ -2,8 +2,12 @@ package com.assiz.cursos.imageliteapi.domain.service;
 
 import com.assiz.cursos.imageliteapi.domain.entity.Image;
 
+import java.util.Optional;
+
 public interface ImageService {
 
     Image save(Image image);
+
+    Optional<Image> getById(String id);
 
 }
